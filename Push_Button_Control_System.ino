1e7e20e721c804ac97d4bf14ec87ae43fdd90592
Ginikachukwu Nwabueze Benedict Ugwumba <morphicalcloud9@gmail.com>
2025-12-22 20:12:29 +0100

    Add files via upload

---
+++ Push_Button_Control_System.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+int buttonState = 0;
+
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  // read the state of the application
+  buttonState = digitalRead(2);
+  // check if pushbutton is pressed. If it is
+  if (buttonState == HIGH) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
